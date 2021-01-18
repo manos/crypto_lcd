eb82e36ccf1cf5a0d6c3a1707884b7e3758905e0
Charlie Schluting <charlie@schluting.com>
2021-01-18 10:54:33 -0600

    fallback to coingecko if coin not listed on coinmarketcap

--- info.py
+++ info.py
@@ -15,7 +15,8 @@ ITER = cycle(CURRENCIES)
 CUR_ITER = []
 CMC_LISTINGS = {}
 CMC_PRO_KEY = os.getenv("CMC_KEY")
-
+CGO_URL = "https://api.coingecko.com/api/v3"
+CGO_COINS = []
 
 def get_json(url):
     """Fetches URL and returns JSON. """
@@ -36,8 +37,8 @@ def btn_1_press_callback(channel):
     lcd.message(create_lcd_str(cycle_next()))
 
 def create_lcd_str(coins):
-    """ Returns string like "BTC: 34,500\nETH: 1200" for first two currencies in [coins] """
-    return f"{coins[0]}: {get_price(coins[0])}\n{coins[1]}: {get_price(coins[1])}"
+    """ Returns string like "BTC: 34,500\nETH: 1200" for first two currencies in [coins], limited to 16 chars """
+    return f"{coins[0]}: {get_price(coins[0])}"[:16] + "\n" + f"{coins[1]}: {get_price(coins[1])}"[:16]
 
 def cycle_next():
     """ Returns two steps on the cycle [a,b], and records current value in CUR_ITER for re-use """
@@ -47,12 +48,33 @@ def cycle_next():
     CUR_ITER = [next1, next2]
     return CUR_ITER
 
+def smart_round(val):
+    """ If we have a number > 0, give 3 decimals, otherwise, give 3 past leading zeroes """
+    if "." not in str(val):
+        return val
+    num, exp = str(val).split(".")
+    if int(num) > 0:
+        return round(val, 3)
+    leading_zeroes = len(exp) - len(exp.strip("0"))
+    return round(val, leading_zeroes + 3)
+    
 def get_price(ticker):
-    """ Returns current price (from coinmarketcap.com) for given symbol """
+    """ Returns current price (from coinmarketcap.com) for given symbol, if listed there, otherwise uses coingecko """
     res = [x["quote"]["USD"]["price"] for x in CMC_LISTINGS["data"] if x["symbol"] == ticker]
     if res:
-        return round(res[0], 3)
-    return 0
+        return smart_round(res[0])
+    else:
+        get_cgo_coins()
+        coin_id = [x["id"] for x in CGO_COINS if x["symbol"] == ticker.lower()][0]
+        res = get_json(CGO_URL + f"/simple/price?ids={coin_id}&vs_currencies=usd")[coin_id]["usd"]
+        return smart_round(res)
+
+@lru_cache(maxsize=None)
+def get_cgo_coins():
+    full_list = get_json(CGO_URL + "/coins/list")
+    # trim to ones we care about
+    global CGO_COINS
+    CGO_COINS = [x for x in full_list if x["symbol"] in [x.lower() for x in CURRENCIES]]
 
 @lru_cache(maxsize=None)
 def update_cmc_data():
